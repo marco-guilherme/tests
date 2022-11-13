4c199919379aef375547a3884fa0bdd83c70e13c
Marco Pereira <marco.pereira@xmobots.com.br>
2022-11-12 23:29:35 -0300

    instação do GitHub Pages adição do .env

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Site do Marco Guilherme</h1>
+      <h2>Versão {process.env.REACT_APP_SITE_VERSION}</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
